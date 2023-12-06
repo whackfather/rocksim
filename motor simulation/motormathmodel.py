66ef5e567c0233284a9f81397a3f80da29bf27ee
WhackFather <147020181+whackfather@users.noreply.github.com>
2023-12-05 20:07:48 -0600

    Update and rename motormathmodel_v3.py to motormathmodel.py

--- motor simulation/motormathmodel.py
+++ motor simulation/motormathmodel.py
@@ -1,37 +1,37 @@
-# Flight Profile of a Rocket
-# Motor Math Model v3
+# Motor Model
+# v2.1
 
 # Importing necessary libraries
 from math import pi
 
 # Setting up initials
-t_step = 0.01  # s
-mass_ox = 46.962  # kg
-mass_fu = 8.538  # kg
-p_tank = 5516000  # pa
-thrust_avg = 7000  # N
-rad_port = 0.063  # m
-a_port = pi * (rad_port ** 2)  # m^2
-isp = 180  # s
-grav = 9.81  # m/s^2
-mdot_ox = (thrust_avg / (isp * grav)) / (1 + (1 / 6.5))  # kg/s
-hgt_fu = 1.07553898867  # m
-rho_fu = (0.871 * 901) + (0.129 * 1220)  # kg/m^3
-burn_time = 13.99  # s
-a = 0.417  # const
-n = 0.347  # const
+t_step = 0.01
+mass_ox = 70 * (6.5 / 7.5)
+mass_fu = mass_ox / 6.5
+pres_tank = 5516000
+avg_thrust = 5374.17
+rad_port = 0.063
+area_port = pi * (rad_port ** 2)
+isp = 180
+grav = 9.81
+mdot_ox = (avg_thrust / (isp * grav)) / (1 + (1 / 6.5))
+a = 0.417
+n = 0.347
+rho_fuel = (0.871 * 901) + (0.129 * 1220)
+vol_grain = mass_fu / rho_fuel
+hgt_grain = vol_grain / (pi * (rad_port ** 2))
 
 # Setting up variables
 t_cur = 0
-port_area = a_port
+port_area = area_port
 port_radius = rad_port
-surface_area = 2 * pi * port_radius * hgt_fu
+surface_area = 2 * pi * port_radius * hgt_grain
 if t_cur < (mass_ox / mdot_ox):
-	ox_flux = mdot_ox / a_port
+	ox_flux = mdot_ox / area_port
 else:
 	ox_flux = 0
 reg_rate = a * ((ox_flux / 10) ** n)
-fuel_mdot = rho_fu * surface_area * (reg_rate / 1000)
+fuel_mdot = rho_fuel * surface_area * (reg_rate / 1000)
 fuel_mass = mass_fu - (fuel_mdot * t_cur)
 ox_mass = mass_ox - (mdot_ox * t_cur)
 prop_mass = fuel_mass + ox_mass
@@ -60,8 +60,8 @@ while True:
 	reg_rate = a * ((ox_flux / 10) ** n)
 	port_radius += ((reg_rate / 1000) * t_step)
 	port_area = pi * (port_radius ** 2)
-	surface_area = 2 * pi * port_radius * hgt_fu
-	fuel_mdot = rho_fu * surface_area * (reg_rate / 1000)
+	surface_area = 2 * pi * port_radius * hgt_grain
+	fuel_mdot = rho_fuel * surface_area * (reg_rate / 1000)
 	if mass_fu - (fuel_mdot * t_cur) > fuel_mass:
 		fuel_mass = fuel_mass
 	else:
